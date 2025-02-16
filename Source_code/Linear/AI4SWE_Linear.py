1fbbbc004f7093079c138986cc86af7a9caf23d2
Amin <71018515+Amin-Nadimy@users.noreply.github.com>
2025-02-16 20:08:28 +0000

    Updated the licience

--- Source_code/Linear/AI4SWE_Linear.py
+++ Source_code/Linear/AI4SWE_Linear.py
@@ -4,16 +4,6 @@
  Imperial College London
 
  amin.nadimy19@imperial.ac.uk
- 
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation,
- version 3.0 of the License.
-
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
 '''
 #-- Import general libraries
 import os
